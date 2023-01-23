b92c3e028e5457f035beea2443ac2a4a98a12f59
Amrulla Khan Patan <amrulla.patan@gmail.com>
2023-01-23 14:05:00 +0530

    Creating a simple product REST API - GetProducts

---
+++ product-api/go.mod
@@ -0,0 +1,3 @@
+module github.com/amrullakhan/go-microservices/product-api
+
+go 1.19
---
+++ product-api/handlers/products.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/amrullakhan/go-microservices/product-api/data"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.GetProducts(w, r)
+	}
+}
+
+func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+
+	err := lp.ToJSON(w)
+
+	if err != nil {
+		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
+		return
+	}
+}
